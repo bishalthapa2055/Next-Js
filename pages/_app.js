56406c02bd3dc738a2bb3a26d2fb98448633993a
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-11-08 13:19:56 +0545

    :rocket: initial commit :rocket:

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component 
+  {...pageProps}
+  />
+  <footer>
+      <p>@2023 Bishalk thyapa</p>
+      <p> Striclty legimate qnaltity</p>
+  </footer>
+  </>
+  )
 }
---
+++ pages/hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "../styles/Hello.module.css"
+const Hello = () => {
+  return (
+    <div className={styles.container}>Hello World</div>
+  );
+};
+
+export default Hello;
