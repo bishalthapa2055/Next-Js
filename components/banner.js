3b5accd8323e85291a48d32423a6f87038a1fe40
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-11-14 15:58:32 +0545

    added images hero section

--- components/banner.js
+++ components/banner.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import styles from "@/styles/Banner.module.css"
+import Image from 'next/image'
 const Banner = () => {
   return (
     <div className='container'>
       
       <div className={styles.banner__container}>
         <div className={styles.banner__subcontainer}>
-
-
-      <h1 className={styles.banner__title}>Coffee 
-      <span className={styles.banner__span}>
-         Conneisser
-        </span>
-         </h1>
-        <h2 className={styles.banner__subtitle}>
-          View All the necessay cointents !!!
-        </h2>
-        <div className={styles.banner__button__container}>
-
-        <button className={styles.banner__button}>View NearBy Store</button>
+          <h1 className={styles.banner__title}>Coffee 
+            <span className={styles.banner__span}>
+            Conneisser
+            </span>
+          </h1>
+          <h2 className={styles.banner__subtitle}>
+              View All the necessay cointents !!!
+          </h2>
+          <div className={styles.banner__button__container}>
+          <button className={styles.banner__button}>View NearBy Store</button>
+          </div>
+        </div>
+        <div className={styles.banner__image__container}>
+          <Image src="/static/hero_image.png"width={400} height={500} className={styles.banner__image}/>
         </div>
         </div>
-      </div>
     </div>
   )
 }
