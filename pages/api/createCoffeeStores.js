1725efaf89a9b4b77003e2925f3817adcf0ebc7a
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-12-01 17:20:38 +0545

    basic change

--- pages/api/createCoffeeStores.js
+++ pages/api/createCoffeeStores.js
@@ -1,6 +1,26 @@
+import CoffeeStore from "@/pages/coffee-store/[id]";
 import Airtable from "airtable";
 const  base = new Airtable({apiKey: process.env.airtable_api_key}).base(process.env.airtable_base_id);
 
 
 
 const table = base('coffee-stores');
+
+console.log(table)
+
+
+const createCoffeeStore = (req, res ) =>{
+
+   if(req.method === 'POST'){
+    res.json({
+        message :"post xa  hai"
+    })
+   }
+   else{
+    res.json({
+        message :"Others"
+    })
+   }
+}
+
+export default createCoffeeStore
